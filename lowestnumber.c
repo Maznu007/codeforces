947e897ef530c8487cbd569ec711d5a1bfb4ceba
Hasnat Rafi Uddin Munif <95755986+Maznu007@users.noreply.github.com>
2025-02-16 21:41:56 +0600

    Add files via upload

---
+++ lowestnumber.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for (int i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+        
+    }
+    int min = arr[0];
+    int place = 1 ;
+    for (int i = 0; i < n; i++) {
+        if (arr[i]<min){
+            min = arr[i];
+            place = i+1;
+        }
+    }
+    printf("%d %d", min, place);
+return 0;}
